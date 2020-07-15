e95180d30ceebeda81f8784c9610f103a2eb5309
PedroD <pedro3manjarrez@hotmail.com>
2020-07-15 12:45:08 -0500

    fix

---
+++ client/components/admin/ui/carousel/Carousel.jsx
@@ -0,0 +1,66 @@
+
+import React from 'react';
+import './Carousel.css';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: "id-cs-" + Math.floor((Math.random() * 100000))
+        }
+        this.mouseEnter = this.mouseEnter.bind(this);
+        this.mouseLeave = this.mouseLeave.bind(this);
+    }
+
+    mouseEnter = () => {
+        $("#" + this.state.id).carousel({
+            interval: 350,
+            cycle: true,
+            pause: "false"
+        }).carousel(0);
+        console.log("Enter");
+    }
+
+
+    mouseLeave = () => {
+        $("#" + this.state.id).carousel(0)
+        console.log("Leave");
+        $("#" + this.state.id).carousel({
+            pause: false,
+            interval: "false"
+
+        });
+        $("#" + this.state.id).carousel("pause");
+
+    }
+
+
+    render() {
+        return (
+            <div id={this.state.id} className="carousel slide" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} data-pause={this.props.pause ? this.props.pause : "false"}>
+                <div className="carousel-inner">
+                    {
+                        this.props.items.map(file => {
+                            return (
+                                <div className={(file.includes("-1.png") ? "active " : "") + "carousel-item"} key={Math.random()}>
+                                    <LazyLoadImage
+                                        effect="blur"
+                                        alt={file}
+                                        visibleByDefault={file.includes("-1.png")}
+                                        src={file} />
+                                </div>);
+                        }, this)
+                    }
+                </div>
+            </div>
+        );
+    }
+
+
+}
+
+
+export default Carousel;
--- client/components/public/movie/Movie.jsx
+++ client/components/public/movie/Movie.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Movie.css';
 import { Link } from 'react-router-dom'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Carousel from '../../admin/ui/carousel/Carousel';
 
 
 class Movie extends React.Component {
@@ -27,110 +27,15 @@ class Movie extends React.Component {
       </>
 
     } else {
-      var id = "id-ss-" + Math.floor((Math.random() * 10));
-      let tr;
-      let hoverAction = () => {
-        debugger
-        if (tr)
-          clearInterval(tr)
-        let sl = document.getElementById(id);
-        let couter = 0;
-        tr = setInterval(() => {
-          switch (couter) {
-            case 1:
-              sl.style.left = 0;
-              break;
 
-            case 2:
-              sl.style.left = "-100%";
-              break;
-            case 3:
-              sl.style.left = "-100%";
-              break;
-            case 4:
-              sl.style.left = "-200%";
-              break;
-            case 5:
-              sl.style.left = "-200%";
-              break;
-            case 6:
-              sl.style.left = "-300%";
-              break;
-            case 7:
-              sl.style.left = "-300%";
-              break;
-            case 8:
-              sl.style.left = "-300%";
-              break;
-            case 9:
-              sl.style.left = "-400%";
-              break;
-            case 10:
-              sl.style.left = "-400%";
-              break;
-            case 11:
-              sl.style.left = "-500%";
-              break;
-            case 12:
-              sl.style.left = "-500%";
-              break;
-            case 13:
-              sl.style.left = "-600%";
-              break;
-            case 14:
-              sl.style.left = "-600%";
-              break;
-            case 15:
-              sl.style.left = "-700%";
-              break;
-            case 16:
-              sl.style.left = "-700%";
-              break;
-            case 17:
-              sl.style.left = "-800%";
-              break;
-            case 18:
-              sl.style.left = "-800%";
-              break;
-            case 19:
-              sl.style.left = "-900%";
-              break;
-            case 20:
-              sl.style.left = "-900%";
-              break;
-
-          }
-          couter++;
-        }, 350);
-
-      }
-      let hoverActionLeave = () => {
-        if (tr)
-          clearInterval(tr)
-        let sl = document.getElementById(id);
-        sl.style.left = "0"
-      }
-      return <div className="slider-carousel"/* onMouseEnter={hoverAction} onMouseLeave={hoverActionLeave}*/>
-        <div id={id} className="figure">
-          {
-            this.props.item.files.map(file => {
-              return (
-                <span key={Math.random()} className="img-span">
-                  <LazyLoadImage
-                    effect="blur"
-                    alt={file}
-                    visibleByDefault={file.includes("-1.png")}
-                    src={file} />
-                </span>);
-            }, this)
-          }
-        </div>
+      return <div className="w-100" >
+        <Carousel items={this.props.item.files} />
         <span className="row-stamp" >
           <span className="text-white time-stamp" >{this.getTime(this.props.item.duration)}</span>
 
           <span className="text-white quality-stamp" >{this.props.item.quality}p</span>
         </span>
-      </div>
+      </div >
     }
   }
 
