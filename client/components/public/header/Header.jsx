d40a9a0436e2dae9a90541075cfab17688cab69c
PedroD <pedro3manjarrez@hotmail.com>
2020-07-26 11:49:44 -0500

    fix

--- client/components/public/header/Header.jsx
+++ client/components/public/header/Header.jsx
@@ -13,7 +13,7 @@ class Header extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">MovieJS</a>
+                <a className="navbar-brand" href="/">MovieJS</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
--- src/utils/db.js
+++ src/utils/db.js
@@ -7,6 +7,7 @@ class Database {
     }
 
     _connect() {
+        console.log(process.env.MONGODB_NAME )
         mongoose.connect(`mongodb://${process.env.MONGODB_URI || "127.0.0.1:27017"}/${process.env.MONGODB_NAME || "movies-api"}`, {
             useCreateIndex: true,
             useUnifiedTopology: true,
