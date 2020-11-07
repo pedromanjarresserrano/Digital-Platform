427cfaaf34acf6b36da5755ff1f0a767f0ebfe54
Pedro D. Manjarres S <pedro3manjarrez@hotmail.com>
2020-11-06 23:27:25 -0500

    fix

--- src/utils/db.js
+++ src/utils/db.js
@@ -8,24 +8,24 @@ class Database {
 
     _connect() {
         console.log(process.env.MONGODB_NAME)
-        mongoose.connect(`mongodb://${process.env.MONGODB_URI || "127.0.0.1:27017"}/${process.env.MONGODB_NAME || "movies-api"}`, {
+        mongoose.connect(rocess.env.MONGODB_URI_FULL || (`mongodb://${process.env.MONGODB_URI || "127.0.0.1:27017"}/${process.env.MONGODB_NAME || "movies-api"}`), {
             useCreateIndex: true,
             useUnifiedTopology: true,
             useNewUrlParser: true
         })
             .then(async (db) => {
-                console.log('Database connection successful')
-                var count = await Users.count().exec();
-                console.log(count);
-                if (count == 0) {
-                    Users.create({
-                        name: "admin",
-                         username: "test@test.com",
-                        email: "test@test.com",
-                        password: "1234"
-                    })
-                }
-            })
+            console.log('Database connection successful')
+            var count = await Users.count().exec();
+            console.log(count);
+            if (count == 0) {
+                Users.create({
+                    name: "admin",
+                    username: "test@test.com",
+                    email: "test@test.com",
+                    password: "1234"
+                })
+            }
+        })
             .catch(err => {
                 console.error('Database connection error')
             })
