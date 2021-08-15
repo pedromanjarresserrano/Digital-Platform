545a842a72c2ae4d854e1ae1bb4f40fa8a75827a
Pedro D. Manjarres S <pedro3manjarrez@hotmail.com>
2021-08-15 02:05:56 -0500

    fix

--- client/src/components/public/movies/movie-details/MovieDetails.jsx
+++ client/src/components/public/movies/movie-details/MovieDetails.jsx
@@ -15,6 +15,8 @@ import { Link } from 'react-router-dom'
 import Actor from '../../actors/actor/Actor';
 import { segFormat } from '../../../../utils/Utils';
 import { Constants } from '../../common/Constants';
+import PanoramicButtom from '../../ui/panoramicbutton/PanoramicButtom';
+
 class MovieDetails extends React.Component {
 
   constructor(props) {
@@ -26,6 +28,7 @@ class MovieDetails extends React.Component {
       muted: vol == 0,
       vol: vol
     }
+    this.panoramic = this.panoramic.bind(this);
   }
 
 
@@ -54,123 +57,128 @@ class MovieDetails extends React.Component {
   }
 
 
-  regresar = () => {
-    this.props.history.goBack();
-  }
+  panoramic = () => {
+    $('.c-player').toggleClass('container-md')
+    $('.c-player').toggleClass('mw-95')
+    window.scrollTo({
+      top: $('.c-player').position().top,
+      behavior: 'smooth'
+    })
 
 
 
-  render() {
-    /*    <button onClick={this.regresar}>Regresar </button>*/
-    const { item } = this.state;
-    if (!item._id)
-      return null;
-    return (
-      <div className="container-md container-fluid">
-        <div className="row">
-          <div className="col-sm-12">
-            <Player fluid={false} width={"100%"} height={480} loop={false} poster={this.state.item.files[1]} ref={(player) => { this.player = player }}>
-              <source src={"/api/movie/" + this.state.item._id} />
-              <BigPlayButton position="center" />
-              <ControlBar autoHide={true}>
-                <ReplayControl seconds={5} order={2.1} />
-                <ForwardControl seconds={5} order={3.1} />
-                <CurrentTimeDisplay order={4.1} />
-                <TimeDivider order={4.2} />
-                <VolumeMenuButton />
-                <PlaybackRateMenuButton rates={Constants.PUBLIC.RATES} order={7.1} />
-              </ControlBar>
-            </Player>
+    render() {
+
+      const { item } = this.state;
+      if (!item._id)
+        return null;
+      return (
+        <div className="container-md container-fluid">
+          <div className="row">
+            <div className="col-sm-12">
+              <Player fluid={true} width={"100%"} height={480} loop={false} poster={this.state.item.files[1]} ref={(player) => { this.player = player }}>
+                <source src={"/api/movie/" + this.state.item._id} />
+                <BigPlayButton position="center" />
+                <ControlBar autoHide={true}>
+                  <ReplayControl seconds={5} order={2.1} />
+                  <ForwardControl seconds={5} order={3.1} />
+                  <CurrentTimeDisplay order={4.1} />
+                  <TimeDivider order={4.2} />
+                  <VolumeMenuButton />
+                  <PlaybackRateMenuButton rates={Constants.PUBLIC.RATES} order={7.1} />
+                  <PanoramicButtom order={8} onClick={this.panoramic} />
+                </ControlBar>
+              </Player>
+            </div>
           </div>
-        </div>
-        <div className="row content padding-top-1">
-          <div className="poster col-sm-12 col-md-3 col-lg-3" >
-            <img className="img-fluid"
-              src={this.state.item.portada} />
-          </div>
-          <div className="movie-body col-sm-12 col-md-9 col-lg-9">
-            <div className="header">
-              <div className="title grid-container">
-                <div className="row">
-                  <div className="col-sm-12 col-md-9 col-lg-10">
-                    <h4 style={{ wordWrap: 'break-word' }}>{this.state.item.visualname ? this.state.item.visualname : this.state.item.name}</h4>
+          <div className="row content padding-top-1">
+            <div className="poster col-sm-12 col-md-3 col-lg-3" >
+              <img className="img-fluid"
+                src={this.state.item.portada} />
+            </div>
+            <div className="movie-body col-sm-12 col-md-9 col-lg-9">
+              <div className="header">
+                <div className="title grid-container">
+                  <div className="row">
+                    <div className="col-sm-12 col-md-9 col-lg-10">
+                      <h4 style={{ wordWrap: 'break-word' }}>{this.state.item.visualname ? this.state.item.visualname : this.state.item.name}</h4>
+                    </div>
+                    <div className="col-sm-12 col-md-9 col-lg-2">
+                      <i className="fab fa-facebook-square fa-2x padding-left-1"></i>
+                      <i className="fab fa-google-plus fa-2x padding-left-1" aria-hidden="true"></i>
+                      <i className="fab fa-twitter-square fa-2x padding-left-1" aria-hidden="true"></i>
+                    </div>
+                    <div className="col-sm-12 ">
+                      <span className="classification">{segFormat(this.state.item.duration)} | {this.state.item.year ? this.state.item.year : '-'}</span>
+                    </div>
                   </div>
-                  <div className="col-sm-12 col-md-9 col-lg-2">
-                    <i className="fab fa-facebook-square fa-2x padding-left-1"></i>
-                    <i className="fab fa-google-plus fa-2x padding-left-1" aria-hidden="true"></i>
-                    <i className="fab fa-twitter-square fa-2x padding-left-1" aria-hidden="true"></i>
-                  </div>
-                  <div className="col-sm-12 ">
-                    <span className="classification">{segFormat(this.state.item.duration)} | {this.state.item.year ? this.state.item.year : '-'}</span>
+                </div>
+              </div>
+              <div className="body grid-container">
+                <div className="row information">
+                  <div className="col-sm-12">
+                    <div className="sinopsis">
+                      {this.state.item.description ? this.state.item.description : "No description"}
+                    </div>
+                    <div className="questions flex-container flex-dir-row">
+                      <div className="list-star flex-child-auto align-self-middle">
+                        <span className="fa fa-star checked"></span>
+                        <span className="fa fa-star checked"></span>
+                        <span className="fa fa-star checked"></span>
+                        <span className="fa fa-star checked"></span>
+                        <span className="fa fa-star"></span>
+                      </div>
+                      <div className="watch padding-2">
+                        <i className="fas fa-eye margin-right-1"></i>
+                        {this.state.item.view ? this.state.item.view : 0} Visitas
+                      </div>
+                      <div className="like padding-2">
+                        <i className="fas fa-heart margin-right-1"></i>
+                        {this.state.item.like ? this.state.item.like : 0} Me gusta
+                      </div>
+                      <div className="comment padding-2">
+                        <i className="fas fa fa-comments margin-right-1"></i>
+                        0 Comentarios
+                      </div>
+                    </div>
+                    <div className="info-gratitude margin-bottom-1 w-100">
+                      <div className="directed w-100">
+                        <span>Título original: </span>
+                        <span className='w-100' style={{ wordWrap: 'break-word' }}>{this.state.item.name ? this.state.item.name : ''}</span>
+                      </div>
+                      <div className="directed w-100">
+                        <span>Director: </span>
+                        <span className='w-100' style={{ wordWrap: 'break-word' }}>{this.state.item.like ? this.state.item.like : ''}</span>
+                      </div>
+                      <div className="credits w-100">
+                        <span>Guión: </span>
+                        <span>Dan Berendsen</span>
+                      </div>
+                      <div className="studio w-100">
+                        <span>Estudio: </span>
+                        <span>{this.state.item.studio ? <Link className="badge badge-pill badge-dark text-white" to={"/studios/studio/" + this.state.item.studio.name}>{this.state.item.studio.name}</Link> : ''}</span>
+                      </div>
+                    </div>
+                    <div className="category w-100">
+                      {this.state.item.categorias ? this.state.item.categorias.map(cat => (<Link key={cat._id} to={"/categories/categorie/" + cat.name} className="badge badge-pill badge-dark">  {cat.name}</Link>)) : ''}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="body grid-container">
-              <div className="row information">
-                <div className="col-sm-12">
-                  <div className="sinopsis">
-                    {this.state.item.description ? this.state.item.description : "No description"}
-                  </div>
-                  <div className="questions flex-container flex-dir-row">
-                    <div className="list-star flex-child-auto align-self-middle">
-                      <span className="fa fa-star checked"></span>
-                      <span className="fa fa-star checked"></span>
-                      <span className="fa fa-star checked"></span>
-                      <span className="fa fa-star checked"></span>
-                      <span className="fa fa-star"></span>
-                    </div>
-                    <div className="watch padding-2">
-                      <i className="fas fa-eye margin-right-1"></i>
-                      {this.state.item.view ? this.state.item.view : 0} Visitas
-                    </div>
-                    <div className="like padding-2">
-                      <i className="fas fa-heart margin-right-1"></i>
-                      {this.state.item.like ? this.state.item.like : 0} Me gusta
-                    </div>
-                    <div className="comment padding-2">
-                      <i className="fas fa fa-comments margin-right-1"></i>
-                      0 Comentarios
-                    </div>
-                  </div>
-                  <div className="info-gratitude margin-bottom-1 w-100">
-                    <div className="directed w-100">
-                      <span>Título original: </span>
-                      <span className='w-100' style={{ wordWrap: 'break-word' }}>{this.state.item.name ? this.state.item.name : ''}</span>
-                    </div>
-                    <div className="directed w-100">
-                      <span>Director: </span>
-                      <span className='w-100' style={{ wordWrap: 'break-word' }}>{this.state.item.like ? this.state.item.like : ''}</span>
-                    </div>
-                    <div className="credits w-100">
-                      <span>Guión: </span>
-                      <span>Dan Berendsen</span>
-                    </div>
-                    <div className="studio w-100">
-                      <span>Estudio: </span>
-                      <span>{this.state.item.studio ? <Link className="badge badge-pill badge-dark text-white" to={"/studios/studio/" + this.state.item.studio.name}>{this.state.item.studio.name}</Link> : ''}</span>
-                    </div>
-                  </div>
-                  <div className="category w-100">
-                    {this.state.item.categorias ? this.state.item.categorias.map(cat => (<Link key={cat._id} to={"/categories/categorie/" + cat.name} className="badge badge-pill badge-dark">  {cat.name}</Link>)) : ''}
-                  </div>
-                </div>
+          </div>
+          <h4 className="text-white">Reparto</h4>
+          <div className="row ">
+            {this.state.item.reparto ? this.state.item.reparto.map(reparto => (
+              <div className="w-20">
+                <Actor item={reparto} />
               </div>
-            </div>
+            )) : ''}
           </div>
         </div>
-        <h4 className="text-white">Reparto</h4>
-        <div className="row ">
-          {this.state.item.reparto ? this.state.item.reparto.map(reparto => (
-            <div className="w-20">
-              <Actor item={reparto} />
-            </div>
-          )) : ''}
-        </div>
-      </div>
-    );
+      );
+    }
+
+
   }
-
-
-}
-export default MovieDetails;
+  export default MovieDetails;
